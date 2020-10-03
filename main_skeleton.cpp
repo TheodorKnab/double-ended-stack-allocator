54647fa3ad08ef011b23bbacd9380b45c2540658
TheodorKnab <32791723+TheodorKnab@users.noreply.github.com>
2020-10-03 15:03:02 +0200

    Free() Canary implementation, and some Test Case Ideas

--- main_skeleton.cpp
+++ main_skeleton.cpp
@@ -6,9 +6,11 @@
 #include "stdio.h"
 #include <cstdlib>
 #include <cstdint>
+#include <ostream>
+#include <iostream>
 
 #define OFFSET_SIZE sizeof(uint32_t)
-
+#define CANARY_SIZE sizeof(uint32_t)
 namespace Tests
 {
 	void Test_Case_Success(const char* name, bool passed)
@@ -54,11 +56,117 @@ namespace Tests
 
 		return true;
 	}
+
+	// Free Success
+
+	template<class A>
+	bool VerifyFreeSuccess(A& allocator, size_t size, size_t alignment)
+	{
+		void* mem = allocator.Allocate(size, alignment);
+
+		allocator.Free(mem);
+
+		void* mem2 = allocator.Allocate(size, alignment);
+
+		if (mem != mem2)
+		{
+			printf("[Error]: Allocate -> Free -> Allocate did not return the same address for first and second allocation!\n");
+			return false;
+		}
+
+		return true;
+	}
+	
+	// Allocate Back Success
+	template<class A>
+	bool VerifyAllocationBackSuccess(A& allocator, size_t size, size_t alignment)
+	{
+		void* mem = allocator.AllocateBack(size, alignment);
+
+		if (mem == nullptr)
+		{
+			printf("[Error]: Allocator returned nullptr!\n");
+			return false;
+		}
+
+		return true;
+	}
+	
+	// Free Back Success
+
+	template<class A>
+	bool VerifyFreeBackSuccess(A& allocator, size_t size, size_t alignment)
+	{
+		void* mem = allocator.Allocate(size, alignment);
+
+		allocator.Free(mem);
+
+		void* mem2 = allocator.Allocate(size, alignment);
+
+		if (mem != mem2)
+		{
+			printf("[Error]: Allocate Back -> Free Back -> Allocate Back did not return the same address for first and second allocation!\n");
+			return false;
+		}
+
+		return true;
+	}
+	
+	// Allocate Align Success
+	   
+	// Allocate Back Align Success
+	   
+	// Allocate -> Allocate (different Alignment) -> Free -> Free
+	   
+	// Allocate Back -> Allocate Back (different Alignment) -> Free Back -> Free Back
+	   
+	// Allocate -> Reset -> Allocate
+	   
+	// Allocate Back -> Reset -> Allocate Back
+	   
+	// Free out of bounds
+	   
+	// Free nullptr
+	   
+	// Free Back out of bounds
+	   
+	// Free Back nullptr
+	   
+	// Free Empty Allocator
+	   
+	// Free Back Empty Allocator
+	   
+	// Allocate MaxSize
+	   
+	// Allocate Back MaxSize
+	   
+	// Allocate Front & Back Half Size
+	   
+	// Allocate Front into Back Memory
+	   
+	// Allocate Back into Front Memory
+	   
+	// Allocate uint_32
+	   
+	// Allocate back uint_32
+	   
+	// Overwrite Canaries Front (Modify DEADBEEF)
+	   
+	// Overwrite Canaries Back (Modify DEADBEEF)
+	   
+	// Alignment + Size > max_size
+	   
+	// Allocate with Size 0
+	   
+	// Allocate with Alignment 0
+
+	// Initialize Allocator with max_size <= 12
+		
 }
 
 // Assignment functionality tests are going to be included here 
 
-#define WITH_DEBUG_CANARIES 0
+//#define WITH_DEBUG_CANARIES 0
 
 /**
 * You work on your DoubleEndedStackAllocator. Stick to the provided interface, this is
@@ -78,28 +186,46 @@ public:
 		m_current_back = m_end;
 	}
 
+	// Overhead of 4 bytes normally
+	// Overhead of 12 bytes with debug canaries
 	void* Allocate(size_t size, size_t alignment)
 	{
-#ifdef WITH_DEBUG_CANARIES
-
-
+		uint32_t total_allocation_size = size + OFFSET_SIZE;
+		uint32_t front_memory_offset = OFFSET_SIZE;
 		
+#ifdef WITH_DEBUG_CANARIES
+		total_allocation_size += CANARY_SIZE * 2;
+		front_memory_offset += CANARY_SIZE;
 #endif
-
+		
 		// If there is not enough memory, we return a nullptr
-		if (!FitsInAllocator(size + OFFSET_SIZE, alignment)) return nullptr;
+		if (!FitsInAllocator(total_allocation_size, alignment)) return nullptr;
 
 		uint32_t offset = reinterpret_cast<char*>(m_current) - reinterpret_cast<char*>(m_begin);
+		
 		// Align address up with offset added, then revert offset so the actual user data is aligned
-		m_current = AlignUp(reinterpret_cast<char*>(m_current) + OFFSET_SIZE, alignment) - OFFSET_SIZE;
-		void* allocated_address = reinterpret_cast<char*>(m_current) + OFFSET_SIZE;
+		m_current = AlignUp(reinterpret_cast<char*>(m_current) + front_memory_offset, alignment) - front_memory_offset;		
+		void* allocated_address = reinterpret_cast<char*>(m_current) + front_memory_offset;
 		
 		// Store offset in first bytes
 		uint32_t* m_current_uint = reinterpret_cast<uint32_t*>(m_current);
 		*m_current_uint = offset;
 
 		//set m_current to new address
-		m_current = reinterpret_cast<char*>(m_current) + size + OFFSET_SIZE;
+		m_current = reinterpret_cast<char*>(m_current) + front_memory_offset  + size;
+
+#ifdef WITH_DEBUG_CANARIES
+		//place DEADBEEF at the front of the allocated memory
+		uint32_t* front_debug_canaray = m_current_uint + 1;
+		*front_debug_canaray = 0xDEADBEEF;
+		
+		//place DEADBEEF at the back of the allocated memory and adjust m_current
+		uint32_t* back_debug_canaray = reinterpret_cast<uint32_t*>(m_current);
+		*back_debug_canaray = 0xDEADBEEF;
+
+		//adjust m_current
+		m_current = back_debug_canaray + 1;
+#endif
 
 		return allocated_address;
 	}
@@ -184,14 +310,18 @@ int main()
 		//Tests::Test_Case_Success("Allocate() returns nullptr", allocator.Allocate(32, 1) == nullptr);
 
 		// Test Case 1 - Assertion that allocator aligns correctly
-		DoubleEndedStackAllocator testcase1(1024u);
-		void* tc1_address1 = allocator.AllocateBack(6, 4);
-		void* tc1_address2 = allocator.AllocateBack(6, 4);
-		allocator.FreeBack(tc1_address2);
-		uint32_t result = reinterpret_cast<char*>(tc1_address2) - reinterpret_cast<char*>(tc1_address1);
-		bool tc1_condition = (result == 4);
-		Tests::Test_Case_Success("Allocate aligns correctly", tc1_condition);
+		//DoubleEndedStackAllocator testcase1(1024u);
+		//void* tc1_address1 = allocator.Allocate(6, 4);
+		//void* tc1_address2 = allocator.Allocate(6, 4);
+		//uint32_t result = reinterpret_cast<char*>(tc1_address2) - reinterpret_cast<char*>(tc1_address1);
+		//bool tc1_condition = (result == 4);
+		//Tests::Test_Case_Success("Allocate aligns correctly", tc1_condition);
 		// -------------------------------------------------------
+		//Tests::Test_Case_Success("Allocate() does not return nullptr", [&allocator]() { return Tests::VerifyAllocationBackSuccess(allocator, 32, 5); }());
+		Tests::Test_Case_Success("Allocate() does not return nullptr", Tests::VerifyAllocationSuccess(allocator, 32, 5));
+		Tests::Test_Case_Success("AllocateBack() does not return nullptr", Tests::VerifyAllocationBackSuccess(allocator, 32, 5));
+		Tests::Test_Case_Success("Free() resets to the correct address", Tests::VerifyFreeSuccess(allocator, 32, 5));
+		Tests::Test_Case_Success("FreeBack() resets to the correct address", Tests::VerifyFreeBackSuccess(allocator, 32, 5));
 	}
 
 	// You can do whatever you want here in the main function
